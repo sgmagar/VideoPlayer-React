7ff7a8d0d4107e15e73f77dcb812d6866999a505
Saurav <sp.gharti@gmail.com>
2017-07-27 21:20:23 +0545

    load videolist and video

--- src/index.js
+++ src/index.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyB13U91WuGEtsR9T9pDtWaYoYErMW6mFSM';
 
-YTSearch({key:API_KEY, term: 'surfboards'}, function(data){
-	console.log(data)
-});
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+class App extends Component{
+	
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+		YTSearch({key:API_KEY, term: 'surfboards'}, (videos) => {
+			// works when key and value are same..such as { videos: videos }
+			this.setState({ videos })
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoDetail video={this.state.videos[0]} />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
 }
 
 
