eba33746ad4b20a1cbdb88ee3fce233601caf87d
Saurav <sp.gharti@gmail.com>
2017-07-27 22:10:01 +0545

    activate search and throttle search with lodash

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
@@ -19,7 +20,11 @@ class App extends Component{
 			selectedVideo: null 
 		};
 
-		YTSearch({key:API_KEY, term: 'surfboards'}, (videos) => {
+		this.videoSearch('surfboards')
+	}
+
+	videoSearch(term){
+		YTSearch({key:API_KEY, term: term}, (videos) => {
 			// works when key and value are same..such as { videos: videos }
 			this.setState({ 
 				videos: videos,
@@ -30,9 +35,11 @@ class App extends Component{
 	}
 
 	render() {
+		const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
 		return (
 			<div>
-				<SearchBar />
+				<SearchBar onSearchTermChange={videoSearch} />
 				<VideoDetail video={this.state.selectedVideo} />
 				<VideoList 
 					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
